3e26756c489bf9a57bd69225174a3fe59b5aafb8
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2022-02-15 14:44:48 +0530

    Added Hover

--- src/components/NewToDisney.js
+++ src/components/NewToDisney.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import Wrap from './effects/Hover';
 import { Button ,Navbar,Container,Row,Col,Card} from 'react-bootstrap'
 
 const NewToDisney=()=>{
@@ -8,24 +9,32 @@ const NewToDisney=()=>{
             <h4 className="text-light pt-4">New to Disney+</h4>
                 <Row xs={2} md={4} className="py-2">
                     <Col>
-                        <Card border="light" style={{ background: "transparent" }}>
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale9.jpg" />
                         </Card>
+                        </Wrap>
                     </Col>
                     <Col>
-                        <Card border="light" style={{ background: "transparent" }}>
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale15.jpg" />
                         </Card>
+                        </Wrap>
                     </Col>
                     <Col className="mt-3 mt-md-0">
-                        <Card border="light" style={{ background: "transparent" }}>
+                        <Wrap>
+                        <Card  style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/badging3.jpg" />
                         </Card>
+                        </Wrap>
                     </Col>
                     <Col className="mt-3 mt-md-0">
-                        <Card border="light" style={{ background: "transparent" }}>
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale13.jpg" />
                         </Card>
+                        </Wrap>
                     </Col>
                 </Row>
             </Container>
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Wrap from './effects/Hover';
 import { Button ,Navbar,Container,Col,Card,Row} from 'react-bootstrap'
 
 const Trending=()=>{
@@ -6,27 +7,35 @@ const Trending=()=>{
         <div>
             <Container>
             <h4 className="text-light bg-md-dark pt-3">Trending</h4>
-                <Row xs={2} md={4} className="py-2">
-                    <div className="col">
-                        <Card border="light" style={{ background: "transparent" }}>
+                <Row xs={2} md={4} className="py-3">
+                    <Col>
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale5.jpg" />
                         </Card>
-                    </div>
-                    <div className="col">
-                        <Card border="light" style={{ background: "transparent" }}>
+                        </Wrap>
+                    </Col>
+                    <Col>
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale18.jpg" />
                         </Card>
-                    </div>
-                    <div className="col" className="mt-3 mt-md-0">
-                        <Card border="light" style={{ background: "transparent" }}>
+                        </Wrap>
+                    </Col>
+                    <Col className="mt-3 mt-md-0">
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale4.jpg" />
                         </Card>
-                    </div>
-                    <div className="col" className="mt-3 mt-md-0">
-                        <Card border="light" style={{ background: "transparent" }}>
+                        </Wrap>
+                    </Col>
+                    <Col className="mt-3 mt-md-0">
+                        <Wrap>
+                        <Card style={{ background: "transparent" }}>
                             <Card.Img variant="top" src="/assets/images/scale14.jpg" />
                         </Card>
-                    </div>
+                        </Wrap>
+                    </Col>
                 </Row>
             </Container>
         </div>
