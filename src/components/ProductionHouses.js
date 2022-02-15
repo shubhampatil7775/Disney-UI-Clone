150533ac0d7ea7c602063d9db0df9e04fcd60844
anshumansinghtw <anshuman.singh@thoughtworks.com>
2022-02-15 10:44:24 +0530

    Added Hover Effects

--- src/components/ProductionHouses.js
+++ src/components/ProductionHouses.js
@@ -1,41 +1,53 @@
 import React from 'react'
-import { Button ,Navbar,Container,Row,Col,Card} from 'react-bootstrap'
+import Wrap from './effects/Hover';
+import { Button, Navbar, Container, Row, Col, Card } from 'react-bootstrap'
 
-const ProductionHouses=()=>{
-    return(
+const ProductionHouses = () => {
+    return (
         <div>
-             <Container>
+            <Container>
                 <div xs={1} md={5} className="py-0 py-md-3 row">
                     <div className="mt-3 mt-md-0 col">
-                        <Card border="light" style={{ background: "transparent" }}>
-                            <Card.Img variant="top" src="/assets/images/scale17.jpg" />
-                        </Card>
+                        <Wrap>
+                            <Card style={{ background: "transparent" }}>
+                                <Card.Img variant="top" src="/assets/images/scale17.jpg" />
+                            </Card>
+                        </Wrap>
                     </div>
                     <div className="mt-3 mt-md-0 col">
-                        <Card border="light" style={{ background: "transparent" }}>
-                            <Card.Img variant="top" src="/assets/images/scale10.jpg" />
-                        </Card>
+                        <Wrap>
+                            <Card style={{ background: "transparent" }}>
+                                <Card.Img variant="top" src="/assets/images/scale10.jpg" />
+                            </Card>
+                        </Wrap>
                     </div>
                     <div className="mt-3 mt-md-0 col">
-                        <Card border="light" style={{ background: "transparent" }}>
-                            <Card.Img variant="top" src="/assets/images/scale7.jpg" />
-                        </Card>
+                        <Wrap>
+                            <Card style={{ background: "transparent" }}>
+                                <Card.Img variant="top" src="/assets/images/scale7.jpg" />
+                            </Card>
+                        </Wrap>
                     </div>
                     <div className="mt-3 mt-md-0 col">
-                        <Card border="light" style={{ background: "transparent" }}>
-                            <Card.Img variant="top" src="/assets/images/scale2.jpg" />
-                        </Card>
+                        <Wrap>
+                            <Card style={{ background: "transparent" }}>
+                                <Card.Img variant="top" src="/assets/images/scale2.jpg" />
+                            </Card>
+                        </Wrap>
                     </div>
                     <div className="mt-3 mt-md-0 col">
-                        <Card border="light" style={{ background: "transparent" }}>
-                            <Card.Img variant="top" src="/assets/images/scale8.jpg" />
-                        </Card>
+                        <Wrap>
+                            <Card style={{ background: "transparent" }}>
+                                <Card.Img variant="top" src="/assets/images/scale8.jpg" />
+                            </Card>
+                        </Wrap>
                     </div>
                 </div>
-                </Container>
-           
+            </Container>
+
         </div>
     )
 }
+
 
 export default ProductionHouses;
