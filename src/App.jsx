b4c53f1bb0528111b443a9bea4391c1a51e4abe6
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2022-02-15 09:48:23 +0530

    Added Production House List

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MenuBar from './components/MenuBar';
 import ImageSlider from './components/ImageSlider';
+import ProductionHouses from './components/ProductionHouses';
 
 function App() {
 	return (
 		<div className="App">
 			<MenuBar />
-			<ImageSlider />
+			<ImageSlider/>
+			<ProductionHouses/>
 		</div>
 	)
 }
---
+++ src/components/ProductionHouses.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Button ,Navbar,Container,Row,Col,Card} from 'react-bootstrap'
+
+const ProductionHouses=()=>{
+    return(
+        <div>
+             <Container>
+                <div xs={1} md={5} className="py-0 py-md-3 row">
+                    <div className="mt-3 mt-md-0 col">
+                        <Card border="light" style={{ background: "transparent" }}>
+                            <Card.Img variant="top" src="/assets/images/scale17.jpg" />
+                        </Card>
+                    </div>
+                    <div className="mt-3 mt-md-0 col">
+                        <Card border="light" style={{ background: "transparent" }}>
+                            <Card.Img variant="top" src="/assets/images/scale10.jpg" />
+                        </Card>
+                    </div>
+                    <div className="mt-3 mt-md-0 col">
+                        <Card border="light" style={{ background: "transparent" }}>
+                            <Card.Img variant="top" src="/assets/images/scale7.jpg" />
+                        </Card>
+                    </div>
+                    <div className="mt-3 mt-md-0 col">
+                        <Card border="light" style={{ background: "transparent" }}>
+                            <Card.Img variant="top" src="/assets/images/scale2.jpg" />
+                        </Card>
+                    </div>
+                    <div className="mt-3 mt-md-0 col">
+                        <Card border="light" style={{ background: "transparent" }}>
+                            <Card.Img variant="top" src="/assets/images/scale8.jpg" />
+                        </Card>
+                    </div>
+                </div>
+                </Container>
+           
+        </div>
+    )
+}
+
+export default ProductionHouses;
